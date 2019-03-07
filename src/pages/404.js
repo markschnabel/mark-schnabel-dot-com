78b7abcbb3a9ce34c31790025027e089fb2b272d
Mark Schnabel <markschnabelcs@gmail.com>
2019-03-07 12:19:55 -0500

    Modified custom error page
    	Modified the error page to use custom content as opposed to the
    	default. Basically just contains a giant 404 and a go back home link now.

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,32 @@
 import React from 'react'
-import Layout from '../components/layout/Layout'
+import styled from 'react-emotion'
+import { Link } from 'gatsby'
+
+const Wrapper = styled.div`
+  ${tw`text-center`}
+`
+const Styled404 = styled.h1`
+${tw`font-black text-background text-center`};
+  font-size: 25vh;
+  text-shadow: -2px 0 #aaa, 0 2px #aaa, 2px 0 #aaa, 0 -2px #aaa;
+`
+const Whoops = styled.p`
+  ${tw`text-3xl font-bold`};
+`
+const Message = styled.p`
+  ${tw`text-xl font-light italic`};
+`
+const StyledLink = styled(Link)`
+  ${tw`text-accent text-xl`};
+`
 
 const NotFoundPage = () => (
-  <Layout>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
+  <Wrapper>
+    <Styled404>404</Styled404>
+    <Whoops>Whoops!</Whoops>
+    <Message>This website only has one page!</Message>
+    <StyledLink to="/">Click here to go back</StyledLink>
+  </Wrapper>
 )
 
 export default NotFoundPage
