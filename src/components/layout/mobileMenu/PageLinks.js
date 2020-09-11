0e2e2ef8e78b49779deddaa8592cd9a7d95cbda5
Mark <mark.schnabel@markschnabel.com>
2020-09-11 03:35:26 -0400

    Reduce duplicate code in scroll links
    possible TODO: introduce application config helper to further reduce duplicate code
    by sharing the scroll configuration for the desktop navbar and the mobile menu links

--- src/components/layout/mobileMenu/PageLinks.js
+++ src/components/layout/mobileMenu/PageLinks.js
@@ -6,59 +6,40 @@ import { Link as SmoothScrollLink } from 'react-scroll';
 const StyledList = styled.ul`
   ${tw`list-reset text-center pt-48 m-auto`};
 `;
+
 const StyledLink = styled(SmoothScrollLink)`
   ${tw`text-3xl my-32 cursor-pointer font-semibold tracking-extra-wide hover:text-accent`};
   transition: color 0.5s ease;
 `;
 
 const PageLinks = ({ closeMobileMenu }) => {
+  const SCROLL_CONFIG = {
+    spy: true,
+    smooth: true,
+    duration: 500,
+    offset: -70,
+    onClick: closeMobileMenu
+  };
+
   return (
     <StyledList>
       <li>
-        <StyledLink
-          to="experienceSection"
-          spy={true}
-          smooth={true}
-          duration={500}
-          offset={-70}
-          onClick={closeMobileMenu}
-        >
+        <StyledLink to="experienceSection" {...SCROLL_CONFIG}>
           EXPERIENCE
         </StyledLink>
       </li>
       <li>
-        <StyledLink
-          to="portfolioSection"
-          spy={true}
-          smooth={true}
-          duration={500}
-          offset={-70}
-          onClick={closeMobileMenu}
-        >
+        <StyledLink to="portfolioSection" {...SCROLL_CONFIG}>
           PORTFOLIO
         </StyledLink>
       </li>
       <li>
-        <StyledLink
-          to="aboutSection"
-          spy={true}
-          smooth={true}
-          duration={500}
-          offset={-70}
-          onClick={closeMobileMenu}
-        >
+        <StyledLink to="aboutSection" {...SCROLL_CONFIG}>
           ABOUT
         </StyledLink>
       </li>
       <li>
-        <StyledLink
-          to="contactSection"
-          spy={true}
-          smooth={true}
-          duration={500}
-          offset={-70}
-          onClick={closeMobileMenu}
-        >
+        <StyledLink to="contactSection" {...SCROLL_CONFIG}>
           CONTACT
         </StyledLink>
       </li>
--- src/components/layout/navbar/Links.js
+++ src/components/layout/navbar/Links.js
@@ -5,58 +5,45 @@ import { Link as SmoothScrollLink } from 'react-scroll';
 const StyledList = styled.ul`
   ${tw`list-reset hidden lg:inline-block`};
 `;
+
 const StyledListItem = styled.ul`
   ${tw`list-reset inline-block`};
 `;
+
 const StyledScrollLink = styled(SmoothScrollLink)`
   ${tw`font-light tracking-extra-wide text-lg ml-6 cursor-pointer hover:text-accent`};
   transition: all 0.5s ease;
 `;
 
+const SCROLL_CONFIG = {
+  spy: true,
+  smooth: true,
+  duration: 500,
+  offset: -70
+};
+
 const Links = () => {
   return (
     <StyledList>
       <StyledListItem>
-        <StyledScrollLink
-          to="experienceSection"
-          spy={true}
-          smooth={true}
-          duration={500}
-          offset={-70}
-        >
+        <StyledScrollLink to="experienceSection" {...SCROLL_CONFIG}>
           EXPERIENCE
         </StyledScrollLink>
       </StyledListItem>
       <StyledListItem>
-        <StyledScrollLink
-          to="portfolioSection"
-          spy={true}
-          smooth={true}
-          duration={500}
-          offset={-70}
+        <StyledScrollLink to="portfolioSection" {...SCROLL_CONFIG}
         >
           PORTFOLIO
         </StyledScrollLink>
       </StyledListItem>
       <StyledListItem>
-        <StyledScrollLink
-          to="aboutSection"
-          spy={true}
-          smooth={true}
-          duration={500}
-          offset={-70}
+        <StyledScrollLink to="aboutSection" {...SCROLL_CONFIG}
         >
           ABOUT
         </StyledScrollLink>
       </StyledListItem>
       <StyledListItem>
-        <StyledScrollLink
-          to="contactSection"
-          spy={true}
-          smooth={true}
-          duration={500}
-          offset={-70}
-        >
+        <StyledScrollLink to="contactSection" {...SCROLL_CONFIG}>
           CONTACT
         </StyledScrollLink>
       </StyledListItem>
