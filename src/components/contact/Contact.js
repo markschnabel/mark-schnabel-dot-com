c9c52711fbd87e7b71b8c8a893f6b18ae99c69a9
Mark Schnabel <mark.schnabel@markschnabel.com>
2023-09-01 16:24:12 -0400

    Removing the contact form from the site
    The contact form no longer works due to gmail updates
    and probably no one actually uses this over a regular email client anyways
    so getting rid of it

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import { Grid, Row, Col } from 'react-flexbox-grid';
+import Fade from 'react-reveal/Fade';
 
 import SectionHeader from '../shared/SectionHeader';
 import ContactLinks from './ContactLinks';
-import ContactForm from './ContactForm';
+import styled from '@emotion/styled';
+
+const Header = styled.h2`
+  ${tw`text-6xl m-2`};
+`;
 
 const Contact = () => {
   return (
-    <div style={{ marginTop: '90px' }}>
+    <div style={{ marginTop: '5rem', marginBottom: '90px' }}>
       <SectionHeader headerText="Contact" />
       <Grid>
         <Row>
           <Col lg={6}>
-            <ContactForm />
+            <Fade left delay={50} distance="50px">
+              <Header>Let's build</Header>
+            </Fade>
+            <Fade right delay={50} distance="50px">
+              <Header>something</Header>
+            </Fade>
           </Col>
           <Col lg={6}>
             <ContactLinks />
