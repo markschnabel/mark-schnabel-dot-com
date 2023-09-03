302163e3ce4755182b0b6c89b2a49f5e80299027
Mark Schnabel <mark.schnabel@markschnabel.com>
2023-09-03 15:32:01 -0400

    Updating content again

--- src/components/about/Bio.js
+++ src/components/about/Bio.js
@@ -64,7 +64,7 @@ const Bio = ({ professionalBio, personalBio }) => {
         </div>
         <div>
           <ResumeLink
-            href="https://drive.google.com/file/d/1g3fJK0l-ONGL7uxsm7snaepwa1oTRvgO/view?usp=sharing"
+            href="https://drive.google.com/file/d/1gFIOl2FB78QCDlLfV1fZKAjQ4sxJs3Oe/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
           >
